f9a15d09a49d114cff5f734df5121a877e0a090a
jonasrk <jonasrk@users.noreply.github.com>
2019-12-10 12:26:08 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ echo "#################################################"
 echo "Starting PyLint"
 
 
-sh - c "python3 -m pylint -E **/*.py"
+sh -c "python3 -m pylint -E **/*.py"
 
 echo "#################################################"
 echo "Ended PyLint"
